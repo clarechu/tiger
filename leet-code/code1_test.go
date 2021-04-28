bfe2ffbd1bbf72cc4d1697090d1cb42749675cdd
Lei Chu <1062186165@qq.com>
2021-04-28 09:29:10 +0800

    树的遍历

--- leet-code/code1_test.go
+++ leet-code/code1_test.go
@@ -17,6 +17,7 @@ package leet_code
 
 import (
 	"fmt"
+	"math"
 	"strings"
 	"testing"
 )
@@ -52,6 +53,50 @@ func Test(t *testing.T) {
 		},
 	}
 	fmt.Printf("%+v", isSameTree(qt, pt))
+
+}
+
+func Test2(t *testing.T) {
+	//fmt.Println(lengthOfLongestSubstring("qrsvbspk"))
+	qt := &TreeNode{
+		Val: 2,
+		Right: &TreeNode{
+			Val: 3,
+			Right: &TreeNode{
+				Val: 5,
+				Right: &TreeNode{
+					Val: 9,
+				},
+				Left: &TreeNode{
+					Val: 8,
+				},
+			},
+			Left: &TreeNode{
+				Val:   4,
+				Right: nil,
+				Left:  nil,
+			},
+		},
+		Left: &TreeNode{
+			Val: 3,
+			Right: &TreeNode{
+				Val: 4,
+				Right: &TreeNode{
+					Val: 8,
+				},
+				Left: &TreeNode{
+					Val: 9,
+				},
+			},
+			Left: &TreeNode{
+				Val:   5,
+				Right: nil,
+				Left:  nil,
+			},
+		},
+	}
+
+	fmt.Printf("%+v", maxDepth(qt))
 
 }
 
@@ -99,15 +144,6 @@ func lengthOfLongestSubstring(s string) int {
 
 	}
 	return mlen
-}
-
-/**
- * Definition for a binary tree node.
- */
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
 }
 
 func isSameTree(p *TreeNode, q *TreeNode) bool {
@@ -167,4 +203,290 @@ func isSymmetrics(q *TreeNode, p *TreeNode) bool {
 		return false
 	}
 	return isSymmetrics(q.Right, p.Left) && isSymmetrics(q.Left, p.Right)
+}
+
+func isSymmetric1(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	queue := NewQueue()
+	queue.push(root.Left)
+	queue.push(root.Right)
+	stack := make([]int, 0)
+	for queue.size() != 0 {
+		cq := NewQueue()
+		for queue.size() != 0 {
+			i := queue.pop().(*TreeNode)
+			if i == nil {
+				stack = append(stack, -1)
+				continue
+			}
+			cq.push(i.Left)
+			cq.push(i.Right)
+			stack = append(stack, i.Val)
+		}
+		if !isa(stack) {
+			return false
+		}
+		stack = make([]int, 0)
+		queue = cq
+	}
+	return true
+}
+
+func isa(a []int) bool {
+	if len(a)&1 != 0 {
+		return false
+	}
+	n := len(a) - 1
+	for i := len(a) / 2; i < len(a); i++ {
+		if a[i] != a[n-i] {
+			return false
+		}
+	}
+	return true
+}
+
+type a struct {
+	max int
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	cur := 0
+	x := a{}
+	dfs(root, &x, cur)
+	return x.max
+}
+
+func dfs(root *TreeNode, max *a, cur int) {
+	cur++
+	if max.max < cur {
+		max.max = cur
+	}
+	if root.Left != nil {
+		dfs(root.Left, max, cur)
+	}
+
+	if root.Right != nil {
+		dfs(root.Right, max, cur)
+	}
+}
+
+func Test3(t *testing.T) {
+	tree := sortedArrayToBST([]int{1, 2, 3, 4, 5, 6, 0, 8})
+	fmt.Printf("%+v", tree)
+	fmt.Printf("%+v", isBalanced(&TreeNode{
+		Val: 1,
+		Right: &TreeNode{
+			Val: 3,
+			Left: &TreeNode{
+				Val: 6,
+			},
+			Right: nil,
+		},
+		Left: &TreeNode{
+			Val: 2,
+			Right: &TreeNode{
+				Val: 5,
+			},
+			Left: &TreeNode{
+				Val:   4,
+				Right: nil,
+				Left: &TreeNode{
+					Val: 8,
+				},
+			},
+		},
+	}))
+
+	fmt.Printf("%+v", lowestCommonAncestor(&TreeNode{
+		Val: 6,
+		Right: &TreeNode{
+			Val: 8,
+			Left: &TreeNode{
+				Val: 7,
+			},
+			Right: &TreeNode{
+				Val: 9,
+			},
+		},
+		Left: &TreeNode{
+			Val: 2,
+			Right: &TreeNode{
+				Val: 4,
+				Right: &TreeNode{
+					Val: 5,
+
+				},
+				Left: &TreeNode{
+					Val:   3,
+				},
+			},
+			Left: &TreeNode{
+				Val:   0,
+			},
+		},
+	}, &TreeNode{Val: 2}, &TreeNode{Val: 4}))
+}
+
+/*
+https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree/
+*/
+func sortedArrayToBST(nums []int) *TreeNode {
+	return toBST(nums, 0, len(nums)-1)
+}
+
+func toBST(nums []int, start, end int) (tree *TreeNode) {
+	if start > end {
+		return
+	}
+	i := (end-start)/2 + start
+	tree = &TreeNode{
+		Val: nums[i],
+	}
+	tree.Left = toBST(nums, start, i-1)
+	tree.Right = toBST(nums, i+1, end)
+	return tree
+}
+
+/*
+https://leetcode-cn.com/problems/balanced-binary-tree/
+*/
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	less := make([]int, 0)
+	dfs1(&less, 0, root)
+	min, max := less[0], less[0]
+	for _, i := range less {
+		if min > i {
+			min = i
+		}
+		if max < i {
+			max = i
+		}
+	}
+	if max-min > 1 {
+		return false
+	}
+	return true
+}
+
+func dfs1(less *[]int, d int, root *TreeNode) {
+	d++
+	if root.Left == nil {
+		*less = append(*less, d)
+	} else {
+		dfs1(less, d, root.Left)
+	}
+
+	if root.Right == nil {
+		*less = append(*less, d)
+	} else {
+		dfs1(less, d, root.Right)
+	}
+}
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+	min := math.MaxInt32
+	if root.Left != nil {
+		min = fmin(minDepth(root.Left), min)
+	}
+	if root.Right != nil {
+		min = fmin(minDepth(root.Right), min)
+	}
+	return min + 1
+}
+
+func fmin(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		if targetSum == 0 {
+			return true
+		}
+		return false
+	}
+	a := false
+	if root.Left != nil {
+		a = hasPathSum(root.Left, targetSum-root.Val)
+		if a {
+			return a
+		}
+	}
+	if root.Right != nil {
+		a = hasPathSum(root.Right, targetSum-root.Val)
+		if a {
+			return a
+		}
+	}
+	if root.Left == nil && root.Right == nil {
+		if root.Val == targetSum {
+			return true
+		}
+	}
+	return false
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	p := &TreeNode{}
+	invert(root, p)
+	return p
+}
+
+func invert(root *TreeNode, p *TreeNode) {
+	p.Val = root.Val
+	if root.Left != nil {
+		p.Right = &TreeNode{}
+		invert(root.Left, p.Right)
+	}
+	if root.Right != nil {
+		p.Left = &TreeNode{}
+		invert(root.Right, p.Left)
+	}
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	queue := NewQueue()
+	queue.push(root)
+	for queue.size() != 0 {
+		a := queue.pop().(*TreeNode)
+		m := make(map[int]int, 0)
+		m[a.Val] =1
+		if a.Left != nil {
+			m[a.Left.Val] =1
+			queue.push(a.Left)
+		}
+		if a.Right != nil {
+			m[a.Right.Val] =1
+			queue.push(a.Right)
+		}
+		_, ok := m[p.Val]
+		_, ok2 := m[q.Val]
+		if ok && ok2 {
+			return a
+		}
+	}
+	return nil
 }
