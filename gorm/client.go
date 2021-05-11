676f335f1a4c90ece9ff5774fdf16c679180cdf3
Lei Chu <1062186165@qq.com>
2021-05-11 15:30:51 +0800

    the same tree

--- gorm/client.go
+++ gorm/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
@@ -36,12 +37,15 @@ type Product struct {
 
 type Server struct {
 	db       *gorm.DB
+	sqlDB    *sql.DB
 	consumer *kafka.Consumer
 }
 
 func NewServer(t *tiger) *Server {
+	db, sqlDB := NewDBClient(t)
 	return &Server{
-		db:       NewDBClient(t),
+		db:       db,
+		sqlDB:    sqlDB,
 		consumer: NewMQClient(t),
 	}
 }
@@ -60,6 +64,7 @@ func main() {
 			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
 		}
 	}
+	server.sqlDB.Close()
 	log.Fatal(server.consumer.Close().Error())
 
 }
@@ -74,12 +79,11 @@ func NewMQClient(t *tiger) *kafka.Consumer {
 	if err != nil {
 		panic(err)
 	}
-
 	c.SubscribeTopics([]string{"myTopic", "^aRegex.*[Tt]opic"}, nil)
 	return c
 }
 
-func NewDBClient(t *tiger) *gorm.DB {
+func NewDBClient(t *tiger) (*gorm.DB, *sql.DB) {
 	db, err := gorm.Open(mysql.New(mysql.Config{
 		DSN:                       fmt.Sprintf("%s:%s@tcp(%s:3306)/gorm?charset=utf8&parseTime=True&loc=Local", t.MysqlUsername, t.MysqlPassword, t.MysqlHost), // DSN data source name
 		DefaultStringSize:         256,                                                                                                                         // string 类型字段的默认长度
@@ -93,7 +97,9 @@ func NewDBClient(t *tiger) *gorm.DB {
 	}
 
 	sqlDB, err := db.DB()
-
+	if err != nil {
+		panic(err)
+	}
 	// SetMaxIdleConns 设置空闲连接池中连接的最大数量
 	sqlDB.SetMaxIdleConns(10)
 
@@ -102,21 +108,7 @@ func NewDBClient(t *tiger) *gorm.DB {
 
 	// SetConnMaxLifetime 设置了连接可复用的最大时间。
 	sqlDB.SetConnMaxLifetime(time.Hour)
-	/*
-		// Read
-		var product Product
-		db.First(&product, 1)                 // 根据整形主键查找
-		db.First(&product, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
-
-		// Update - 将 product 的 price 更新为 200
-		db.Model(&product).Update("Price", 200)
-		// Update - 更新多个字段
-		db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // 仅更新非零值字段
-		db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
-
-		// Delete - 删除 product
-		db.Delete(&product, 1)*/
-	return db
+	return db, sqlDB
 }
 
 func (s *Server) CreateProduct(msg string) error {
